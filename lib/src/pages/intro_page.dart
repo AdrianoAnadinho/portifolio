f17f8c438ad127d5e56672e5bbab161ad6abb6e2
Adriano Anadinho <adrianoanadinho@gmail.com>
2022-11-10 15:21:33 -0300

    tabbar alignment

--- lib/src/pages/intro_page.dart
+++ lib/src/pages/intro_page.dart
@@ -30,7 +30,7 @@ class _IntroPageState extends State<IntroPage>
   void initState() {
     super.initState();
 
-    tabController = TabController(length: 2, vsync: this, initialIndex: 0);
+    tabController = TabController(length: 1, vsync: this, initialIndex: 0);
   }
 
   @override
@@ -43,8 +43,15 @@ class _IntroPageState extends State<IntroPage>
         ),
       ),
       child: Scaffold(
-        appBar: true
-            ? TabBar(
+        appBar: PreferredSize(
+          preferredSize: TabBar(
+            tabs: [],
+          ).preferredSize,
+          child: Align(
+            alignment: Alignment.centerRight,
+            child: SizedBox(
+              width: 100,
+              child: TabBar(
                 indicator: BoxDecoration(color: Colors.transparent),
                 indicatorColor: Colors.transparent,
                 labelColor: Colors.transparent,
@@ -53,180 +60,204 @@ class _IntroPageState extends State<IntroPage>
                   return Colors.transparent;
                 }),
                 tabs: [
-                  Tab(
-                    child: Text(''),
-                  )
+                  GestureDetector(
+                    onTap: () => print('go home man'),
+                    child: UnderlineAnimation(
+                      textCubit: homeTextCubit,
+                      text: 'Home',
+                      color: Colors.red,
+                      duration: Duration(milliseconds: 200),
+                      maxWidth: 30,
+                      minWidth: 30,
+                    ),
+                  ),
                 ],
                 controller: tabController,
-              )
-            : PreferredSize(child: Container(), preferredSize: Size.zero),
+              ),
+            ),
+          ),
+        ),
         // AppBar(
         //   actions: [Icon(Icons.lock_clock)],
         //   backgroundColor: Colors.transparent,
         //   elevation: 0,
         // ) ,
         backgroundColor: Colors.transparent,
-        body: LayoutBuilder(builder: (context, constraints) {
-          return SingleChildScrollView(
-            child: SizedBox(
-              height: max(constraints.maxHeight, 200),
-              child: Padding(
-                padding: EdgeInsets.all(MediaQuery.of(context).size.width / 10),
-                child: TabBarView(controller: tabController, children: [
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    mainAxisAlignment: MainAxisAlignment.center,
+        body: LayoutBuilder(
+          builder: (context, constraints) {
+            return SingleChildScrollView(
+              child: SizedBox(
+                height: max(constraints.maxHeight, 200),
+                child: Padding(
+                  padding:
+                      EdgeInsets.all(MediaQuery.of(context).size.width / 10),
+                  child: TabBarView(
+                    controller: tabController,
                     children: [
-                      Padding(
-                        padding: const EdgeInsets.all(8),
-                        child: Row(
-                          children: [
-                            Expanded(
-                              child: Text(
-                                'Adriano Anadinho',
-                                style: TextStyle(
-                                    overflow: TextOverflow.fade,
-                                    fontWeight: FontWeight.bold,
-                                    fontSize: Theme.of(context)
-                                        .textTheme
-                                        .headline2
-                                        ?.fontSize),
-                              ),
-                            )
-                          ],
-                        ),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.all(8),
-                        child: Row(
-                          children: [
-                            Expanded(
-                              child: Text(
-                                'Brazillian developer ',
-                                style: Theme.of(context).textTheme.headline5,
-                              ),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.all(8),
+                            child: Row(
+                              children: [
+                                Expanded(
+                                  child: Text(
+                                    'Adriano Anadinho',
+                                    style: TextStyle(
+                                        overflow: TextOverflow.fade,
+                                        fontWeight: FontWeight.bold,
+                                        fontSize: Theme.of(context)
+                                            .textTheme
+                                            .headline2
+                                            ?.fontSize),
+                                  ),
+                                )
+                              ],
                             ),
-                          ],
-                        ),
-                      ),
-                      SingleChildScrollView(
-                        scrollDirection: Axis.horizontal,
-                        child: Padding(
-                          padding: const EdgeInsets.all(8.0),
-                          child: Row(
-                            children: [
-                              Row(
-                                mainAxisAlignment: MainAxisAlignment.start,
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.all(8),
+                            child: Row(
+                              children: [
+                                Expanded(
+                                  child: Text(
+                                    'Brazillian developer ',
+                                    style:
+                                        Theme.of(context).textTheme.headline4,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                          SingleChildScrollView(
+                            scrollDirection: Axis.horizontal,
+                            child: Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: Row(
                                 children: [
-                                  Padding(
-                                    padding: const EdgeInsets.only(right: 24),
-                                    child: GestureDetector(
-                                      onTap: () => print('go home man'),
-                                      child: UnderlineAnimation(
-                                        textCubit: homeTextCubit,
-                                        text: 'Home',
+                                  Row(
+                                    mainAxisAlignment: MainAxisAlignment.start,
+                                    children: [
+                                      Padding(
+                                        padding:
+                                            const EdgeInsets.only(right: 24),
+                                        child: GestureDetector(
+                                          onTap: () => print('go home man'),
+                                          child: UnderlineAnimation(
+                                            textCubit: homeTextCubit,
+                                            text: 'Home',
+                                            color: Colors.red,
+                                            duration:
+                                                Duration(milliseconds: 200),
+                                            maxWidth: 30,
+                                            minWidth: 30,
+                                          ),
+                                        ),
+                                      ),
+                                      Padding(
+                                        padding:
+                                            const EdgeInsets.only(right: 24),
+                                        child: GestureDetector(
+                                          onTap: () => {
+                                            Navigator.push(
+                                                context,
+                                                MaterialPageRoute(
+                                                  builder: (context) =>
+                                                      MyHomePage(title: 'app'),
+                                                ))
+                                          },
+                                          child: UnderlineAnimation(
+                                            textCubit: aboutTextCubit,
+                                            text: 'About',
+                                            color: Colors.red,
+                                            duration:
+                                                Duration(milliseconds: 200),
+                                            maxWidth: 30,
+                                          ),
+                                        ),
+                                      ),
+                                      Padding(
+                                        padding:
+                                            const EdgeInsets.only(right: 24),
+                                        child: UnderlineAnimation(
+                                          textCubit: resumeTextCubit,
+                                          text: 'Resume',
+                                          color: Colors.red,
+                                          duration: Duration(milliseconds: 200),
+                                          maxWidth: 30,
+                                        ),
+                                      ),
+                                      Padding(
+                                        padding:
+                                            const EdgeInsets.only(right: 24),
+                                        child: UnderlineAnimation(
+                                          textCubit: servicesTextCubit,
+                                          text: 'Services',
+                                          color: Colors.red,
+                                          duration: Duration(milliseconds: 200),
+                                          maxWidth: 30,
+                                        ),
+                                      ),
+                                      Padding(
+                                        padding:
+                                            const EdgeInsets.only(right: 24),
+                                        child: UnderlineAnimation(
+                                          textCubit: portfolioTextCubit,
+                                          text: 'Portfolio',
+                                          color: Colors.red,
+                                          duration: Duration(milliseconds: 200),
+                                          maxWidth: 30,
+                                        ),
+                                      ),
+                                      UnderlineAnimation(
+                                        textCubit: contactTextCubit,
+                                        text: 'Contact',
                                         color: Colors.red,
                                         duration: Duration(milliseconds: 200),
                                         maxWidth: 30,
-                                        minWidth: 30,
                                       ),
-                                    ),
-                                  ),
-                                  Padding(
-                                    padding: const EdgeInsets.only(right: 24),
-                                    child: GestureDetector(
-                                      onTap: () => {
-                                        Navigator.push(
-                                            context,
-                                            MaterialPageRoute(
-                                              builder: (context) =>
-                                                  MyHomePage(title: 'app'),
-                                            ))
-                                      },
-                                      child: UnderlineAnimation(
-                                        textCubit: aboutTextCubit,
-                                        text: 'About',
-                                        color: Colors.red,
-                                        duration: Duration(milliseconds: 200),
-                                        maxWidth: 30,
-                                      ),
-                                    ),
-                                  ),
-                                  Padding(
-                                    padding: const EdgeInsets.only(right: 24),
-                                    child: UnderlineAnimation(
-                                      textCubit: resumeTextCubit,
-                                      text: 'Resume',
-                                      color: Colors.red,
-                                      duration: Duration(milliseconds: 200),
-                                      maxWidth: 30,
-                                    ),
-                                  ),
-                                  Padding(
-                                    padding: const EdgeInsets.only(right: 24),
-                                    child: UnderlineAnimation(
-                                      textCubit: servicesTextCubit,
-                                      text: 'Services',
-                                      color: Colors.red,
-                                      duration: Duration(milliseconds: 200),
-                                      maxWidth: 30,
-                                    ),
-                                  ),
-                                  Padding(
-                                    padding: const EdgeInsets.only(right: 24),
-                                    child: UnderlineAnimation(
-                                      textCubit: portfolioTextCubit,
-                                      text: 'Portfolio',
-                                      color: Colors.red,
-                                      duration: Duration(milliseconds: 200),
-                                      maxWidth: 30,
-                                    ),
-                                  ),
-                                  UnderlineAnimation(
-                                    textCubit: contactTextCubit,
-                                    text: 'Contact',
-                                    color: Colors.red,
-                                    duration: Duration(milliseconds: 200),
-                                    maxWidth: 30,
+                                    ],
                                   ),
                                 ],
                               ),
-                            ],
+                            ),
                           ),
-                        ),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.start,
-                          children: const [
-                            Padding(
-                              padding:
-                                  EdgeInsets.only(right: 16, bottom: 8, top: 8),
-                              child: Icon(FontAwesomeIcons.discord),
+                          Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.start,
+                              children: const [
+                                Padding(
+                                  padding: EdgeInsets.only(
+                                      right: 16, bottom: 8, top: 8),
+                                  child: Icon(FontAwesomeIcons.discord),
+                                ),
+                                Padding(
+                                  padding: EdgeInsets.all(8.0),
+                                  child: Icon(FontAwesomeIcons.reddit),
+                                ),
+                                Padding(
+                                  padding: EdgeInsets.all(8.0),
+                                  child: Icon(FontAwesomeIcons.whatsapp),
+                                ),
+                                Padding(
+                                  padding: EdgeInsets.all(8.0),
+                                  child: Icon(FontAwesomeIcons.github),
+                                )
+                              ],
                             ),
-                            Padding(
-                              padding: EdgeInsets.all(8.0),
-                              child: Icon(FontAwesomeIcons.reddit),
-                            ),
-                            Padding(
-                              padding: EdgeInsets.all(8.0),
-                              child: Icon(FontAwesomeIcons.whatsapp),
-                            ),
-                            Padding(
-                              padding: EdgeInsets.all(8.0),
-                              child: Icon(FontAwesomeIcons.github),
-                            )
-                          ],
-                        ),
+                          ),
+                        ],
                       ),
                     ],
                   ),
-                ]),
+                ),
               ),
-            ),
-          );
-        }),
+            );
+          },
+        ),
       ),
     );
   }
@@ -268,11 +299,15 @@ class UnderlineAnimation extends StatelessWidget {
                     bottom: BorderSide(color: color, width: 2),
                   ),
                 ),
-                child: Text(''),
+                child: Padding(
+                  padding: const EdgeInsets.all(2),
+                  child: Text(''),
+                ),
               ),
               Text(
                 text,
                 style: TextStyle(
+                  fontSize: 16,
                   color: state
                       ? Theme.of(context).colorScheme.secondary
                       : Theme.of(context).colorScheme.primary,
